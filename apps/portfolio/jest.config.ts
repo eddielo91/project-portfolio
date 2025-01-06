6cf915be6cdb48d6ca2b160f01ea071993af6556
Eddie Lo <eddielo106@gmail.com>
2025-01-06 14:26:57 -0800

    Rename project-portfolio -> portfolio

--- apps/portfolio/jest.config.ts
+++ apps/portfolio/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'project-portfolio',
+  displayName: 'portfolio',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/project-portfolio',
+  coverageDirectory: '../../coverage/apps/portfolio',
 };
